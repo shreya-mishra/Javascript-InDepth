e0d94ab7bcbcc225d0011a70f2e070f5f1f732c1
dubey-shivani <shreyamishra040398@gmail.com>
2022-03-03 22:55:56 +0530

    added spread,destructuring and rest paramters

--- Methods/methods.js
+++ Methods/methods.js
@@ -18,3 +18,20 @@ const obj2 = {
 obj.fun.apply(obj2, ["ghr", "UP"]);
 obj.fun.call(obj2, "ghr1", "UP1");
 obj.fun.bind(obj2, "delhi", "up2")();
+
+// SPread , rest and destructuring
+// Destructuring is used to create varibles from array items or object properties
+
+[a, b] = [1, 2];
+console.log(a, b);
+
+// Spread syntax is used to unpack iterables such as arrays, objects, and function calls.
+const arr = [1, 2, 3, 5];
+const result = [...arr];
+// Rest parameter syntax will create an array from an indefinite number of values.
+//instead of unpacking it tied into individual array
+function restTest(...args) {
+  console.log(args);
+}
+
+restTest(1, 2, 3, 4, 5, 6);
